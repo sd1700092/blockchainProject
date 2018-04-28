3de6a9741e6009bd6d0f4bca7f9ca7e50cf9d535
sd1700092 <foreverskyone@hotmail.com>
2018-04-28 21:52:44 +0800

    initial commit

--- blockchain200line/bc200line.go
+++ blockchain200line/bc200line.go
@@ -8,6 +8,10 @@ import (
 	"os"
 	"time"
 	"github.com/gorilla/mux"
+	"encoding/json"
+	"io"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
 )
 
 type Block struct {
@@ -16,6 +20,10 @@ type Block struct {
 	BPM       int
 	Hash      string
 	PrevHash  string
+}
+
+type Message struct {
+	BPM int
 }
 
 var Blockchain []Block
@@ -82,6 +90,61 @@ func makeMuxRouter() http.Handler {
 	return muxRouter
 }
 
-func main() {
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	bytes, err:= json.MarshalIndent(Blockchain, "", "  ")
+	if err!=nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w, string(bytes))
+}
 
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+	var m Message
+	decoder := json.NewDecoder(r.Body)
+	if err:=decoder.Decode(&m); err!=nil{
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], m.BPM)
+	if err!=nil {
+		respondWithJSON(w, r, http.StatusInternalServerError, m)
+		return
+	}
+
+	if isBlockValid(newBlock, Blockchain[len(Blockchain) - 1]){
+		newBlockchain:=append(Blockchain, newBlock)
+		replaceChain(newBlockchain)
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(w,r , http.StatusCreated, newBlock)
+}
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}){
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err!=nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func main() {
+	err := godotenv.Load("resources.env")
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	go func() {
+		t:=time.Now()
+		genesisBlock:=Block{0, t.String(), 0, "", ""}
+		spew.Dump(generateBlock)
+		Blockchain=append(Blockchain, genesisBlock)
+	}()
+	log.Fatal(run())
 }
